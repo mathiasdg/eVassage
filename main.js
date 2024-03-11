e831ea2a75d281e61b913774250d0e023d8c35f3
mathiasdg <mathias@bladsteenschaar.be>
2024-03-11 17:01:36 +0100

    map updated naar mapbox static

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 import '/scss/main.scss'
 
+const staticMapUrl = "https://api.mapbox.com/styles/v1/mapbox/outdoors-v12/static/1.1588,43,12,0/300x200?access_token=pk.eyJ1IjoibWF0aGlmbGlwIiwiYSI6ImNsZXE4cmwyejAxZTQzcHBxdWdqODc3MWYifQ.IFFcSRHH2GwAwPQghdqeGQ"
+
 !function(){window;const e=document.documentElement;if(e.classList.remove("no-js"),e.classList.add("js"),document.body.classList.contains("has-animations")){(window.sr=ScrollReveal()).reveal(".reveal-on-scroll",{duration:600,distance:"20px",easing:"cubic-bezier(0.5, -0.01, 0, 1.005)",origin:"bottom",interval:100})}}();

ee554546b44953e7d2346a7d9d9bd491646b733d
mathiasdg <mathias@bladsteenschaar.be>
2025-02-03 12:15:08 +0100

    build proces geoptimalisseerd en map locatie geupdate

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 // import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import { imagetools } from 'vite-imagetools';
-import injectHTML from 'vite-plugin-html-inject';
+// import { imagetools } from "vite-imagetools";
+import injectHTML from "vite-plugin-html-inject";
 
 export default defineConfig(() => {
-	return {
-		plugins: [
-			imagetools(),
-			injectHTML(),
-			]
-		}
-	});
+  return {
+    base: "./", // Adjust this to match your deployment path
+    plugins: [
+      // imagetools(),
+      injectHTML(),
+    ],
+  };
+});

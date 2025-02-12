b9ea9b754cbeab6b229fcbd383bf35c320d39072
mathiasdg <mathias.de.gucht@gmail.com>
2025-02-12 11:54:38 +0100

    VITE CONSTS komen uit .env nu

--- main.js
+++ main.js
@@ -3,11 +3,6 @@ import quote1 from "./content/quote1.md";
 import quote2 from "./content/quote2.md";
 import info from "./content/info.md";
 
-// const staticMapUrl =
-// "https://api.mapbox.com/styles/v1/mapbox/outdoors-v12/static/1.48836,42.762,17,0/690x690?access_token=pk.eyJ1IjoibWF0aGlmbGlwIiwiYSI6ImNsZXE4cmwyejAxZTQzcHBxdWdqODc3MWYifQ.IFFcSRHH2GwAwPQghdqeGQ";
-// const mapUrl = "https://maps.app.goo.gl/dKzeWtCEsApQAzgB6";
-// document.querySelector("#static-map").src = staticMapUrl;
-
 document.querySelector("#quote1-content").innerHTML = quote1;
 document.querySelector("#quote2-content").innerHTML = quote2;
 document.querySelector("#info-content").innerHTML = info;
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { defineConfig } from "vite";
+import { vitePluginMdToHTML } from "vite-plugin-md-to-html";
 import injectHTML from "vite-plugin-html-inject";
-import {vitePluginMdToHTML} from "vite-plugin-md-to-html";
 
 export default defineConfig(() => {
   return {
-    base: "./", // Adjust this to match your deployment path
+    base: "./",
     plugins: [
       ViteImageOptimizer({
-	    dir: './img/processed',
+        dir: "./img/processed",
         webp: {
           quality: 80,
           progressive: true,
